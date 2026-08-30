c91ce260e94ea134c7d953af9a7aacb101b7a4fe
Davide Berweger Gaillard <davide@davideb.ch>
2026-08-30 05:08:01 +0200

    Improve ifStatus_802F61FC local allocation

--- src/melee/if/ifstatus.c
+++ src/melee/if/ifstatus.c
@@ -706,31 +706,39 @@ check_done:
     return gx_cur;
 }
 
-static inline HSD_GObj* ifStatus_CreateMarkGObj(void)
+inline void ifStatus_CreateMarkGObj(HSD_GObj** gobj)
 {
-    return GObj_Create(0xE, 0xF, 0);
+    *gobj = GObj_Create(0xE, 0xF, 0);
+}
+
+static inline void ifStatus_GetPlayerCharacter(s32 arg0,
+                                                CharacterKind* chara)
+{
+    u8 idx = arg0;
+    *chara = Player_GetPlayerCharacter(idx);
 }
 
 HSD_GObj* ifStatus_802F61FC(IfDamageState* state, s32 player_idx)
 {
     CharacterKind chara;
-    HSD_GObj* gobj;
     HSD_JObj* jobj;
     HSD_TObj* tobj;
     Vec3* vec;
     HSD_MObj* mobj;
-    GXColor color;
-    u8 slot;
-    u8 team;
-    u8 hud_color;
     HudIndex* hud = ifStatus_GetHUDInfo();
+    u8 slot;
+    GXColor color;
+    u8 hud_color;
+    u8 team;
     u8 idx = player_idx;
     PAD_STACK(0x10);
 
-    chara = Player_GetPlayerCharacter(idx);
+    ifStatus_GetPlayerCharacter(player_idx, &chara);
     if (state->next == NULL) {
+        HSD_GObj* gobj;
+
         ifAll_GetArchive();
-        gobj = ifStatus_CreateMarkGObj();
+        ifStatus_CreateMarkGObj(&gobj);
         if (gobj == NULL) {
             HSD_ASSERTREPORT(0x30A, 0,
                              "Error : gobj dont't get (ifAddMark)\n");
